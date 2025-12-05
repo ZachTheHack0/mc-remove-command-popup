a409efb2e810700a683e9c760a0e79ce0bd1523f
Mine The Line <zach.chan8226@gmail.com>
2025-12-05 16:47:27 -0500

    Initial commit

---
+++ src/main/java/net/mcreator/remove_command_popup/RemoveCommandPopupModClient.java
@@ -0,0 +1,11 @@
+package net.mcreator.remove_command_popup;
+
+import net.fabricmc.api.Environment;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.ClientModInitializer;
+
+@Environment(EnvType.CLIENT)
+public class RemoveCommandPopupModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {}
+}
---
+++ src/main/java/net/mcreator/remove_command_popup/mixin/InjectNoGUI.java
@@ -0,0 +1,20 @@
+package net.mcreator.remove_command_popup.mixin;
+
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.*;
+import net.fabricmc.api.*;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.network.packet.c2s.play.CommandExecutionC2SPacket;
+
+@Environment(EnvType.CLIENT)
+@Mixin(net.minecraft.client.network.ClientPlayNetworkHandler.class)
+public class InjectNoGUI {
+	@Overwrite
+	public void runClickEventCommand(String command, @Nullable Screen afterActionScreen) {
+		((ClientPlayNetworkHandler)(Object)this).sendPacket(new CommandExecutionC2SPacket(command));
+        MinecraftClient.getInstance().setScreen(afterActionScreen);
+	}
+}
