9e88aee7a0c763c329f89893e143eb0e29ffd06d
Mine The Line <zach.chan8226@gmail.com>
2025-12-12 16:49:35 -0500

    Converted Yarn mappings to Mojang Mappings

--- src/main/java/net/mine_the_line/remove_command_popup/mixin/InjectNoGUI.java
+++ src/main/java/net/mine_the_line/remove_command_popup/mixin/InjectNoGUI.java
@@ -1,20 +1,24 @@
 package net.mine_the_line.remove_command_popup.mixin;
 
-import org.jetbrains.annotations.Nullable;
-import org.spongepowered.asm.mixin.*;
-import net.fabricmc.api.*;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.network.ClientPlayNetworkHandler;
-import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.network.packet.c2s.play.CommandExecutionC2SPacket;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.multiplayer.ClientPacketListener;
+import net.minecraft.network.protocol.game.ServerboundChatCommandPacket;
+
+import org.jetbrains.annotations.Nullable;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 
 @Environment(EnvType.CLIENT)
-@Mixin(net.minecraft.client.network.ClientPlayNetworkHandler.class)
+@Mixin(net.minecraft.client.multiplayer.ClientPacketListener.class)
 public class InjectNoGUI {
 	@Overwrite
-	public void runClickEventCommand(String command, @Nullable Screen afterActionScreen) {
-		((ClientPlayNetworkHandler)(Object)this).sendPacket(new CommandExecutionC2SPacket(command));
-        MinecraftClient.getInstance().setScreen(afterActionScreen);
+	public void sendUnattendedCommand(String command, @Nullable Screen afterActionScreen) {
+		((ClientPacketListener)(Object)this).send(new ServerboundChatCommandPacket(command));
+        Minecraft.getInstance().setScreen(afterActionScreen);
 	}
 }
